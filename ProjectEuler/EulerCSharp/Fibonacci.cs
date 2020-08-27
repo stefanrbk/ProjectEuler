bc501367cab1823b697d95a25981819bc07f1c74
stefanrbk <stefan.kewatt@gmail.com>
2020-08-27 10:44:16 -0500

    Problem2 (#1)
    * Update .gitignore
    
    * Add Problem 2
    
    * Added Problem002 and confirmed the answer.

---
+++ ProjectEuler/EulerCSharp/Fibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EulerCSharp
+{
+    public class Fibonacci
+    {
+        private (int, int) initialValues;
+        public Fibonacci(int first, int second) =>
+            initialValues = (first - (second - first), second - first);
+
+        public IEnumerator<int> GetEnumerator() =>
+            new FibonacciEnumerator(initialValues);
+    }
+    public sealed class FibonacciEnumerator : IEnumerator<int>
+    {
+        private (int, int) initialValues;
+        private int last;
+        public int Current { get; private set; }
+        object IEnumerator.Current => Current;
+
+        public FibonacciEnumerator((int last, int current) initial)
+        {
+            initialValues = initial;
+            last = initial.last;
+            Current = initial.current;
+        }
+
+        public void Dispose() { }
+        public bool MoveNext()
+        {
+            var value = last + Current;
+            last = Current;
+            Current = value;
+
+            return true;
+        }
+        public void Reset() =>
+            (last, Current) = initialValues;
+    }
+}
