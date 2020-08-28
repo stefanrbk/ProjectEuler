2cef1c2863102d55dc866748406cf259739c5801
stefanrbk <stefan.kewatt@gmail.com>
2020-08-28 12:30:25 -0500

    Added language and problem name to the EulerProblemAttribute and updated the 2 existing problems.

--- ProjectEuler/ProjectTemplates/EulerProblemAttribute.cs
+++ ProjectEuler/ProjectTemplates/EulerProblemAttribute.cs
@@ -5,12 +5,16 @@ namespace ProjectTemplates
     [System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
     public sealed class EulerProblemAttribute : Attribute
     {
+        public readonly Language Language;
         public readonly int ProblemNumber;
+        public readonly string Name;
         public readonly string Description;
 
-        public EulerProblemAttribute(int problemNum, string description)
+        public EulerProblemAttribute(Language language, int problemNum, string name, string description)
         {
+            Language = language;
             ProblemNumber = problemNum;
+            Name = name;
             Description = description;
         }
     }
