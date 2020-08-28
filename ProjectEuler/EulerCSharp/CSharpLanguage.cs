955765b23e7508ca31e8d6d22f03350484795442
stefanrbk <stefan.kewatt@gmail.com>
2020-08-28 16:11:20 -0500

    Added an Attribute to flag the entry point for the language as a whole which discovers all EulerProblemAttributed classes.

---
+++ ProjectEuler/EulerCSharp/CSharpLanguage.cs
@@ -0,0 +1,35 @@
+﻿using ProjectTemplates;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace EulerCSharp
+{
+    [LanguageDescriptor(Language.CSharp)]
+    public static class CSharpLanguage
+    {
+        public static Action InitEntryPoint { get; } = Init;
+        public static string Name => 
+            "C#";
+        public static List<(EulerProblemAttribute Problem, Func<string> EntryPoint)> Problems { get; } = 
+            new List<(EulerProblemAttribute, Func<string>)>();
+
+        private static List<IEulerProblem> problemInstances = new List<IEulerProblem>();
+
+        static void Init()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var types = asm.ExportedTypes;
+            foreach (var type in types
+                .Where(a => !(a.GetCustomAttribute<EulerProblemAttribute>() is null)))
+            {
+                var instance = (IEulerProblem)asm.CreateInstance(type.FullName);
+                problemInstances.Add(instance);
+                Problems.Add((type.GetCustomAttribute<EulerProblemAttribute>(), instance.Run));
+            }
+        }
+    }
+}
---
+++ ProjectEuler/ProjectTemplates/LanguageDescriptorAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTemplates
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class LanguageDescriptorAttribute : Attribute
+    {
+        public Language Language { get; }
+        public LanguageDescriptorAttribute(Language language) => 
+            Language = language;
+    }
+}
