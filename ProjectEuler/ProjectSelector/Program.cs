71207b9797940ef482e6f92a8b47612f8497658b
stefanrbk <stefan.kewatt@gmail.com>
2020-09-02 16:11:26 -0500

    Some refactoring done. More still needed.

--- ProjectEuler/ProjectSelector/Program.cs
+++ ProjectEuler/ProjectSelector/Program.cs
@@ -3,8 +3,7 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Numerics;
-using System.Reflection;
+using System.Threading;
 
 namespace ProjectSelector
 {
@@ -15,56 +14,71 @@ namespace ProjectSelector
             Console.WriteLine("Loading...");
             var locator = new LanguageLocator(Directory.GetCurrentDirectory());
 
+            DisplayLanguages(locator);
+        }
+
+        private static void DisplayLanguages(LanguageLocator locator)
+        {
             var languages = locator.Languages;
-            var languageNames = languages.Select<LanguageHandler, (string Name, int StrLength)>
-                (a => {
-                    var lang = PrintLanguage(a);
-                    return (lang, lang.Length);
-                }).ToList();
+            var languageNames = languages.Select
+                ((Func<LanguageHandler, (string Name, int StrLength)>)(a => (a.Name, a.Name.Length))).ToList();
 
-
-            
             while (true)
             {
-                Console.WriteLine("Select a language\n\n");
+                Console.Clear();
+                Console.WriteLine("Select a language\n");
                 for (var i = 0; i < languageNames.Count; i++)
                 {
                     Console.WriteLine($"{i + 1}: {languageNames[i].Name}");
                 }
-                Console.WriteLine("\nTo exit, type \"Exit\"");
+                Console.WriteLine("\nTo exit, type \"Exit\"\n");
                 var value = Console.ReadLine();
                 if (value.ToLower() == "exit")
                     break;
 
                 if (int.TryParse(value, out var languageNumber) && languageNumber <= languages.Count && languageNumber > 0)
                 {
-                    while (true) 
-                    {
-                        var lang = languages[languageNumber - 1];
-                        foreach (var problem in lang.Problems)
-                        {
-                            Console.WriteLine($"{problem.Number}: {problem.Name}");
-                        }
-                        Console.WriteLine("\nTo exit, type\"Exit\"");
-                        var probValue = Console.ReadLine();
-                        if (probValue.ToLower() == "exit")
-                            break;
-
-                        if (int.TryParse(probValue, out var probNumber) && lang.Problems.Select(a => a.Number).Contains(probNumber))
-                        {
-                            var prob = lang.Problems.Where(a => a.Number == probNumber).First();
-                            Console.WriteLine($"{prob!.Number}: {prob!.Name}");
-                            Console.WriteLine(prob!.Description);
-                            Console.WriteLine("Press any key to begin.");
-                            Console.Read();
-                            Console.WriteLine(prob!.ProjectEulerSolutionEntryPoint());
-                            Console.Read();
-                        }
-                    }
+                    DisplayProblems(languages[languageNumber - 1]);
                     break;
                 }
             }
         }
+
+        private static void DisplayProblems(LanguageHandler language)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"({language.Name}) Select a problem\n");
+                foreach (var problem in language.Problems)
+                {
+                    Console.WriteLine($"{problem.Number}: {problem.Name}");
+                }
+                Console.WriteLine("\nTo exit, type \"Exit\"\n");
+                var probValue = Console.ReadLine();
+                if (probValue.ToLower() == "exit")
+                    break;
+
+                if (int.TryParse(probValue, out var probNumber) && language.Problems.Select(a => a.Number).Contains(probNumber))
+                {
+                    var prob = language.Problems.Where(a => a.Number == probNumber).First();
+                    DisplayProblem(prob);
+                }
+            }
+        }
+
+        private static void DisplayProblem(Problem problem)
+        {
+            Console.Clear();
+            Console.WriteLine($"{problem.Number}: {problem.Name}");
+            Console.WriteLine(problem.Description);
+            Console.WriteLine("\nPress any key to begin.\n");
+            Console.ReadKey(true);
+            Console.WriteLine(problem.ProjectEulerSolutionEntryPoint());
+            Console.WriteLine("\nPress any key to continue.\n");
+            Console.ReadKey(true);
+        }
+
         static string PrintLanguage(LanguageHandler handler) =>
             handler.Name;
     }
